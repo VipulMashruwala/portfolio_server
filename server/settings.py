82d39782ba0ca6ae24461da9ac68a5932e732104
Vipul Mashruwala <mashruwala.vipul@gmail.com>
2023-01-31 22:50:13 +0530

    third_commit

--- server/settings.py
+++ server/settings.py
@@ -134,5 +134,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'https://vipulmashruwala.github.io/vipul_mashruwala1710'
+    'http://vipulmashruwala.github.io/vipul_mashruwala1710',
+    'https://vipulmashruwala.github.io'
+    'http://vipulmashruwala.github.io'
 ]
